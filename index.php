ce0ff2c94fe71fe01b7f0eee5b310124c9d52156
albertogioia93 <albertogioia@hotmail.it>
2023-04-17 15:25:33 +0200

    3 classi ereditanto da class prodotti

--- index.php
+++ index.php
@@ -4,4 +4,23 @@ class Prodotti {
     public $nomeProdotto;
     public $animale;
     public $prezzo;
-}
+};
+
+class Cibo extends Prodotti {
+    public $pesoNetto;
+    public $ingredienti;
+};
+
+class Giochi extends Prodotti {
+    public $caratteristiche;
+    public $dimensioni;
+};
+
+class Accessori extends Prodotti {
+    public $materiale;
+    public $dimensioni;
+};
+
+var_dump(new Cibo());
+var_dump(new Giochi());
+var_dump(new Accessori());
